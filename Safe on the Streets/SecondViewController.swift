e5e020dce077620c37e0de5e13e44c4303ae1a82
ZachJiroun <zjiroun@gmail.com>
2015-11-30 22:14:26 -0500

    Initial Commit

---
+++ Safe on the Streets/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Safe on the Streets
+//
+//  Created by Zach Jiroun on 11/30/15.
+//  Copyright © 2015 Zach Jiroun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
