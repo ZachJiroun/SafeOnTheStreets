3c82e6c44ac7802280befb063b989e700c344b13
ZachJiroun <zjiroun@gmail.com>
2015-12-05 21:30:45 -0500

    update

--- Pods/Target Support Files/BFPaperTabBarController/BFPaperTabBarController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BFPaperTabBarController : NSObject
-@end
-@implementation PodsDummy_BFPaperTabBarController
-@end
--- Pods/Target Support Files/BFPaperTabBarController/BFPaperTabBarController-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "BFPaperTabBarController.h"
-
-FOUNDATION_EXPORT double BFPaperTabBarControllerVersionNumber;
-FOUNDATION_EXPORT const unsigned char BFPaperTabBarControllerVersionString[];
-
